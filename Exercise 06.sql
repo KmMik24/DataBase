d717c7674f976b861f9fef944876c4798ffdc4da
Kamran <m.kamran.pwk@gmail.com>
2020-04-24 19:47:59 +0400

    Page 55

--- Exercise 06.sql
+++ Exercise 06.sql
@@ -18,8 +18,8 @@ FROM STUDENT, EXAM_MARKS
 WHERE mark > 
 	(SELECT AVG(mark) 
 	FROM EXAM_MARKS) 
-		AND STUDENT.student_id = EXAM_MARKS.student_id 
-		AND subj_id = 10;
+	AND STUDENT.student_id = EXAM_MARKS.student_id 
+	AND subj_id = 10;
  
 -- 3
 SELECT DISTINCT firstname 
@@ -27,14 +27,14 @@ FROM STUDENT, EXAM_MARKS
 WHERE mark < 
 	(SELECT AVG(mark) 
 	FROM EXAM_MARKS) 
-		AND STUDENT.student_id = EXAM_MARKS.student_id 
-		AND subj_id = 22;
+	AND STUDENT.student_id = EXAM_MARKS.student_id 
+	AND subj_id = 22;
 
 -- 4
 SELECT student_id, COUNT(subj_id) 
 FROM EXAM_MARKS 
-	GROUP BY student_id 
-	HAVING COUNT(subj_id) > 1;
+GROUP BY student_id 
+HAVING COUNT(subj_id) > 1;
 
 -- 5
 SELECT student_id, firstname, stipend 
@@ -48,16 +48,16 @@ WHERE stipend =
 SELECT student_id, firstname 
 FROM STUDENT A 
 WHERE A.city IS NOT NULL 
-	AND city NOT IN 
-		(SELECT B.city 
-		FROM UNIVERSITY B);
+AND city NOT IN 
+	(SELECT B.city 
+	FROM UNIVERSITY B);
 
 -- 7 (а)
 SELECT DISTINCT student_id, firstname, univ_name, A.city 
 FROM STUDENT A, UNIVERSITY B 
 WHERE A.city < > B.city 
-	AND A.city IS NOT NULL 
-	AND A.univ_id = B.univ_id;
+AND A.city IS NOT NULL 
+AND A.univ_id = B.univ_id;
 
 -- 7 (б)
 SELECT DISTINCT student_id, firstname 
