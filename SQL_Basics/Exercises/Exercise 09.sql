31fdf7b6686d4d3a63ba9a791af25575a9e524b3
Kamran <m.kamran.pwk@gmail.com>
2020-05-06 01:10:43 +0400

    Page 66

--- SQL_Basics/Exercises/Exercise 09.sql
+++ SQL_Basics/Exercises/Exercise 09.sql
@@ -1,18 +1,67 @@
 /*
     SQL В ПРИМЕРАХ И ЗАДАЧАХ.
     ISBN 985-475-004-3
-    Exercises from page 74
+    Exercises from page 66
 */
 
 -- 1
-SELECT univ_name, city, rating, 'Высокий'
+SELECT univ_name, city, 'Высокий'
     FROM UNIVERSITY
     WHERE rating >= 300
 UNION
-SELECT univ_name, city, rating, 'Низкий'
+SELECT univ_name, city, 'Низкий'
     FROM UNIVERSITY
-    WHERE rating < 300;
+    WHERE rating < 300
+ORDER BY 3;
+
 
 -- 2
+SELECT surname, student_id, 'Успевает'
+    FROM STUDENT A
+    WHERE 3 <  ALL 
+        (SELECT mark
+        FROM EXAM_MARKS B
+        WHERE A.student_id = B.student_id)
+UNION
+SELECT surname, student_id, 'Не успевает'
+    FROM STUDENT A
+    WHERE 3 = ANY 
+        (SELECT mark
+        FROM EXAM_MARKS B
+        WHERE A.student_id = B.student_id)
+UNION
+SELECT surname, student_id, 'Не сдавал'
+    FROM STUDENT A
+    WHERE NOT EXISTS
+        (SELECT mark
+        FROM EXAM_MARKS B
+        WHERE A.student_id = B.student_id) 
+ORDER BY 1;
+ 
 
-        
+-- 3
+SELECT surname, firstname, 'Преподаватель' 
+    FROM LECTURER
+    WHERE city = 'Москва'
+UNION
+SELECT surname, firstname, 'Студент'
+    FROM STUDENT
+    WHERE city = 'Москва'
+ORDER BY 3;
+
+
+-- 4
+SELECT surname, firstname, 'Преподаватель' 
+    FROM LECTURER
+    WHERE univ_id = 
+        (SELECT univ_id
+        FROM UNIVERSITY
+        WHERE univ_name = 'ВГУ')
+UNION
+SELECT surname, firstname, 'Студент'
+    FROM STUDENT
+    WHERE univ_id = 
+        (SELECT univ_id
+        FROM UNIVERSITY
+        WHERE univ_name = 'ВГУ')
+ORDER BY 3;
