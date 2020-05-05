bb8ab87aceb09aeb5ecc8b0d233549062015299c
Kamran <m.kamran.pwk@gmail.com>
2020-05-05 23:36:31 +0400

    Update Exercise 09.sql

--- SQL_Basics/Exercises/Exercise 09.sql
+++ SQL_Basics/Exercises/Exercise 09.sql
@@ -1,7 +1,7 @@
 /*
     SQL В ПРИМЕРАХ И ЗАДАЧАХ.
     ISBN 985-475-004-3
-    Exercises from page 74
+    Exercises from page 66
 */
 
 -- 1
@@ -14,5 +14,5 @@ SELECT univ_name, city, rating, 'Низкий'
     WHERE rating < 300;
 
 -- 2
-
+ 
         
