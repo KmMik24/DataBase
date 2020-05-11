66a430a4cca7ffce7f335b696295408a37ea7695
Kamran <m.kamran.pwk@gmail.com>
2020-05-11 22:52:55 +0400

    Page 27 (Data Selection)

--- SQL_Basics/Exercises/Exercise 01.sql
+++ SQL_Basics/Exercises/Exercise 01.sql
@@ -5,37 +5,61 @@
 */
 
 -- 1
-SELECT * FROM SUBJECTS;
+SELECT *
+FROM SUBJECTS;
 
 -- 2
-SELECT * FROM EXAM_MARKS WHERE subj_id = 22;
+SELECT *
+FROM EXAM_MARKS 
+WHERE subj_id = 22;
 
 -- 3
-SELECT kurs, surname, firstname, stipend FROM STUDENT; 
+SELECT kurs, surname, firstname, stipend
+FROM STUDENT; 
 
 -- 4
-SELECT subj_name, hour FROM SUBJECTS WHERE semester = 4;
+SELECT subj_name, hour 
+FROM SUBJECTS 
+WHERE semester = 4;
 
 -- 5
-SELECT DISTINCT mark FROM EXAM_MARKS;
+SELECT DISTINCT mark 
+FROM EXAM_MARKS;
 
 -- 6
-SELECT DISTINCT surname FROM STUDENT WHERE kurs >= 3;
+SELECT DISTINCT surname 
+FROM STUDENT 
+WHERE kurs >= 3;
 
 -- 7
-SELECT surname, firstname, kurs FROM STUDENT WHERE stipend > 140;
+SELECT surname, firstname, kurs 
+FROM STUDENT 
+WHERE stipend > 140;
 
 -- 8
-SELECT subj_name FROM SUBJECTS WHERE hour > 30;
+SELECT subj_name 
+FROM SUBJECTS 
+WHERE hour > 30;
 
 -- 9
-SELECT univ_name FROM UNIVERSITY WHERE rating > 300;
+SELECT univ_name 
+FROM UNIVERSITY 
+WHERE rating > 300;
 
 -- 10
-SELECT surname, firstname, kurs FROM STUDENT where stipend >= 100 AND city = 'Воронеж';
+SELECT surname, firstname, kurs 
+FROM STUDENT 
+WHERE stipend >= 100 
+AND city = 'Воронеж';
 
 -- 11
-SELECT * FROM STUDENT WHERE (stipend < 100 OR NOT (birthday >= '10-03-1980' and student_id > 103));
+SELECT * 
+FROM STUDENT 
+WHERE (stipend < 100 
+OR NOT (birthday >= '10-03-1980' AND student_id > 103));
 
 -- 12
-SELECT * FROM STUDENT where NOT ((birthday = '10-03-1980' or stipend > 100) and student_id >= 103);
+SELECT * 
+FROM STUDENT 
+WHERE NOT ((birthday = '10-03-1980' OR stipend > 100) 
+AND student_id >= 103);
