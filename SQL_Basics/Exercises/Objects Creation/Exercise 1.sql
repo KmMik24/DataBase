98cd3f943bdad7851caae6cce079d6306ecccea0
Kamran <m.kamran.pwk@gmail.com>
2020-05-13 14:45:49 +0400

    Page 89

---
+++ SQL_Basics/Exercises/Objects Creation/Exercise 1.sql
@@ -0,0 +1,18 @@
+/*
+    SQL В ПРИМЕРАХ И ЗАДАЧАХ.
+    ISBN 985-475-004-3
+    Exercises from page 89
+*/
+
+-- 1
+-- 2
+-- 3
+-- 4
+-- 5
+-- Уже сделаны в папке ТАБЛИЦЫ
+
+-- 6
+CREATE INDEX STUD ON STUDENT(kurs);
+
+-- 7
+CREATE INDEX MARK1 ON EXAM_MARKS(exam_date)
